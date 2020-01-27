3466bcd464013e2513ef17bb5cc449d5597fa875
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-27 23:44:20 +0000

    Minor Format + Style Change

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import ItemsList from './components/ItemsList';
-import Item from './components/Item';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import ItemsList from "./components/ItemsList";
+import Item from "./components/Item";
 
 function App() {
-	return (
-		<div className="App">
-			<nav>
-				<Link to="/">
-					<h1 className="store-header">Trinkets</h1>
-				</Link>
-				<div className="nav-links">
-					<Link to="/">Home</Link>
-					<Link className="some-link" to="/items-list">
-						Shop
-					</Link>
-				</div>
-			</nav>
-			<Route exact path="/">
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1 className="store-header">Trinkets</h1>
+        </Link>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link className="some-link" to="/items-list">
+            Shop
+          </Link>
+        </div>
+      </nav>
+      <Route exact path="/">
         <Home />
       </Route>
-			<Route exact path="/items-list">
+      <Route exact path="/items-list">
         <ItemsList />
       </Route>
-			<Route path="/items-list/:itemId">
+      <Route path="/items-list/:itemId">
         <Item />
       </Route>
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
