99968d07341527e125b1bcd60f692b5d6367ee8d
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-28 09:33:29 +0000

    .

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,14 +3,18 @@ import { Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import ItemsList from "./components/ItemsList";
 import Item from "./components/Item";
+import { useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
+  const goHome = () => { 
+    history.push("/");
+  }
+  
   return (
     <div className="App">
       <nav>
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <h1 className="store-header">Trinkets</h1>
-        </Link>
+        <h1 className="store-header" onClick={goHome}>Trinkets</h1>
         <div className="nav-links">
           <Link to="/">Home</Link>
           <Link className="some-link" to="/items-list">
